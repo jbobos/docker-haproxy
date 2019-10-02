4a08ee27c269ed4e4aa677807f0b06dcb0328f1e
Tao XU <tao@jbobos.com>
2019-10-02 16:10:09 +0930

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# docker-haproxy
+# Dockerizing HAProxy (rsyslogd) with Keepalived
+
+### Start containers
+```bash
+# scale to 5 web servers
+docker-compose up -d --scale web=5
+# find the proxy container's IP
+docker inspect proxy | grep IPAddress
+# show logs of the proxy container
+docker logs -f proxy
+```
+
+### Access the proxy container
+```bash
+# Use your IP found from docker inspect command
+curl -v 172.18.0.7
+```
+
---
+++ proxy/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+
+readonly RSYSLOG_PID="/var/run/rsyslogd.pid"
+
+main() {
+  start_rsyslogd
+  start_haproxy "$@"
+}
+
+# make sure we have rsyslogd's pid file not
+# created before
+start_rsyslogd() {
+  rm -f $RSYSLOG_PID
+  rsyslogd
+}
+
+# Starts the load-balancer (haproxy) with 
+# whatever arguments we pass to it ("$@")
+start_haproxy() {
+  exec /docker-entrypoint.sh "$@"
+}
+
+main "$@"
