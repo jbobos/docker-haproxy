f40b578bf947eb7503ff036881e2fc8969d0a3f1
Tao XU <tao@jbobos.com>
2019-10-03 15:46:37 +0930

    refactoring keepalived conf

--- proxy/entrypoint.sh
+++ proxy/entrypoint.sh
@@ -5,7 +5,7 @@ set -o nounset
 
 readonly RSYSLOG_PID="/var/run/rsyslogd.pid"
 readonly KEEPALIVED_PID="/var/run/keepalived"
-readonly KEEPALIVED_CONF="/etc/keepalived/keepalived.$KEEPALIVED_STATE.conf"
+readonly KEEPALIVED_CONF="/etc/keepalived/keepalived.conf"
 
 main() {
   start_keepalived
@@ -15,7 +15,14 @@ main() {
 
 # make sure we have keepalived's pid file not created before
 start_keepalived() {
+  # find the target ip and set it to KEEPALIVED_SRC_IP
+  bind_target="$(ip addr show eth0 | grep -m 1 -E -o 'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | awk '{print $2}')"
+  # set it as KEEPALIVED_SRC_IP
+  sed -i 's/$KEEPALIVED_SRC_IP/'"$bind_target"'/g' $KEEPALIVED_CONF
+
+  # delete keepalived's pid
   rm -rf $KEEPALIVED_PID
+  # start keepalived
   /usr/sbin/keepalived -n -l -D -f $KEEPALIVED_CONF --dont-fork --log-console &
 }
 
@@ -25,7 +32,7 @@ start_rsyslogd() {
   rsyslogd
 }
 
-# Starts the load-balancer (haproxy) with 
+# Starts the load-balancer (haproxy) with
 # whatever arguments we pass to it ("$@")
 start_haproxy() {
   exec /docker-entrypoint.sh "$@"
